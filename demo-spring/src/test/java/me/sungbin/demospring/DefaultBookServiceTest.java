fadd86d75f658b7f693a1e2bb824f8078151889f
Robert.Yang <ysb05222@naver.com>
2021-09-26 12:19:50 +0900

    다이나믹 프록시 실습
    다이나믹 프록시 실습

--- demo-spring/src/test/java/me/sungbin/demospring/DefaultBookServiceTest.java
+++ demo-spring/src/test/java/me/sungbin/demospring/DefaultBookServiceTest.java
@@ -2,15 +2,34 @@ package me.sungbin.demospring;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
 class DefaultBookServiceTest {
 
-    BookService bookService = new BookServiceProxy(new DefaultBookService());
+    BookService bookService = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(), new Class[]{BookService.class}, new InvocationHandler() {
+
+        BookService bookService = new DefaultBookService();
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            if (method.getName().equals("rent")) {
+                System.out.println("aaaaa");
+                Object invoke = method.invoke(bookService, args);
+                System.out.println("bbbb");
+                return invoke;
+            }
+            return method.invoke(bookService, args);
+        }
+    });
 
     @Test
     void di() {
         Book book = new Book();
         book.setTitle("spring");
         bookService.rent(book);
+        bookService.returnBook(book);
     }
 
 }
