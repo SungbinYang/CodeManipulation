b65d91a640a1bca522fc621f4efbf92297b984ee
Robert.Yang <ysb05222@naver.com>
2021-09-26 18:18:04 +0900

    애노태이션 프로세서 실습
    애노태이션 프로세서 실습

---
+++ annomoja/src/main/java/me/sungbin/App.java
@@ -0,0 +1,12 @@
+package me.sungbin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+//        Moja moja = new MagicMoja();
+//        System.out.println(moja.pullOut());
+    }
+}
---
+++ annomoja/src/main/java/me/sungbin/Moja.java
@@ -0,0 +1,7 @@
+package me.sungbin;
+
+@Magic
+public interface Moja {
+
+    String pullOut();
+}
