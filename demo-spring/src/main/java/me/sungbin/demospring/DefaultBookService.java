84497f95d2df2d7d35b4e44a718fb87a7a9d7a4c
Robert.Yang <ysb05222@naver.com>
2021-09-26 12:49:41 +0900

    클래스의 프록시가 필요하다면?
    클래스의 프록시가 필요하다면?

--- demo-spring/src/main/java/me/sungbin/demospring/DefaultBookService.java
+++ demo-spring/src/main/java/me/sungbin/demospring/DefaultBookService.java
@@ -1,12 +1,11 @@
 package me.sungbin.demospring;
 
-public class DefaultBookService implements BookService {
+public class DefaultBookService{
 
     public void rent(Book book) {
         System.out.println("rent: " + book.getTitle());
     }
 
-    @Override
     public void returnBook(Book book) {
         System.out.println("return: " + book.getTitle());
     }
--- demo-spring/src/test/java/me/sungbin/demospring/DefaultBookServiceTest.java
+++ demo-spring/src/test/java/me/sungbin/demospring/DefaultBookServiceTest.java
@@ -1,31 +1,57 @@
 package me.sungbin.demospring;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.InvocationHandlerAdapter;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
 
 class DefaultBookServiceTest {
 
-    BookService bookService = (BookService) Proxy.newProxyInstance(BookService.class.getClassLoader(), new Class[]{BookService.class}, new InvocationHandler() {
-
-        BookService bookService = new DefaultBookService();
-
-        @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if (method.getName().equals("rent")) {
-                System.out.println("aaaaa");
-                Object invoke = method.invoke(bookService, args);
-                System.out.println("bbbb");
-                return invoke;
-            }
-            return method.invoke(bookService, args);
-        }
-    });
-
     @Test
-    void di() {
+    void di() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+//        MethodInterceptor handler = new MethodInterceptor() {
+//
+//            DefaultBookService bookService = new DefaultBookService();
+//
+//            @Override
+//            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+//                if (method.getName().equals("rent")) {
+//                    System.out.println("aaaa");
+//                    Object invoke = method.invoke(bookService, objects);
+//                    System.out.println("bbbb");
+//                    return invoke;
+//                }
+//
+//                return method.invoke(bookService, objects);
+//            }
+//        };
+//        DefaultBookService bookService = (DefaultBookService) Enhancer.create(DefaultBookService.class, handler);
+
+        Class<? extends DefaultBookService> proxyClass = new ByteBuddy().subclass(DefaultBookService.class)
+                .method(named("rent")).intercept(InvocationHandlerAdapter.of(new InvocationHandler() {
+
+                    DefaultBookService bookService = new DefaultBookService();
+
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println("aaaa");
+                        Object invoke = method.invoke(bookService, args);
+                        System.out.println("bbbb");
+                        return invoke;
+                    }
+                }))
+                .make().load(DefaultBookService.class.getClassLoader()).getLoaded();
+
+        DefaultBookService bookService = proxyClass.getConstructor(null).newInstance();
+
         Book book = new Book();
         book.setTitle("spring");
         bookService.rent(book);
